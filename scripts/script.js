282102e8d43a4796cb9d8a3b95441ffa2187df3d
Hidravneno <aramburoeduardo972@gmail.com>
2024-12-11 21:07:42 -0600

    archives

---
+++ scripts/script.js
@@ -0,0 +1,62 @@
+console.log("Script");
+
+//string "" is a text
+let firstName = "Samantha";
+let lastName = "Jimenez";
+
+//numbers
+let myAge = 99;
+let myShoe = 9.5;
+
+//boolean they can be true or false
+let isStudent = false; 
+
+//display
+console.log(firstName);
+console.log(myAge);
+
+//concatenation
+console.log("My name is " + firstName + " and I am " + myAge + " years old and my shoe size is " +myShoe );
+
+let num1 =10; 
+let num2=20;
+
+let addition = num1 + num2;
+let subs = num1 - num2;
+let mult = num1 * num2;
+let div = num1/num2;
+
+console.log("Addition is: " + addition);
+console.log("Substraction is: " + subs);
+console.log("Multiplication is: " + mult);
+console.log("Division is: " + div);
+
+let a;
+const pi= 3.1416;
+let r=3; //=prompt("Enter the radius");
+
+a = pi * (r*r);
+console.log(" The area of a circle with a radius of " + r + " is the " + a);
+
+// define the var
+let aVariable; 
+
+//assign a value
+aVariable=20;
+
+//define and assing the value at the same time
+let otherVariable = 30;
+
+console.log(aVariable,otherVariable);
+
+aVariable = 50;
+otherVariable = 100;
+
+console.log(aVariable,otherVariable);
+
+const weekDays = 7; 
+console.log(weekDays);
+
+// not possible
+//weekDays=5;
+
